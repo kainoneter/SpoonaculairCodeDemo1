6d68f1814dc0522028b21e7c053229126883b632
kainoneter <kain.scheffer@student.graafschapcollege.nl>
2024-09-12 09:24:27 +0200

    changed background and text color, fixed remaining recipe bug

--- recipes.js
+++ recipes.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
-    $('#searchButton').on('click', function () {
-        $.ajax({
-            url: `https://api.spoonacular.com/recipes/complexSearch?query=${$('#searchValue').val()}&apiKey=c25e2d349ba842ee8186ded1ff30b942`
-        }).done(function (data) {
-            data.results.forEach(recipe => {
-                const searchResults = document.querySelector('.searchResults');
-
-                const newItem = document.createElement('div');
-                newItem.classList.add('recipe');
-                newItem.innerHTML = `<h3>${recipe.title}</h3><a href="ingredients.html?id=${recipe.id}"><img src=${recipe.image} /></a>`;
-                searchResults.appendChild(newItem);
-            });
-        });
+  $("#searchButton").on("click", function () {
+    $(".searchResults").empty();
+    $.ajax({
+      url: `https://api.spoonacular.com/recipes/complexSearch?query=${$(
+        "#searchValue"
+      ).val()}&apiKey=c7566f79c0de44f08f602b1d3f8dab1f`,
+    }).done(function (data) {
+      data.results.forEach((recipe) => {
+        const searchResults = document.querySelector(".searchResults");
+        const newItem = document.createElement("div");
+        newItem.classList.add("recipe");
+        newItem.innerHTML = `<h3>${recipe.title}</h3><a href="ingredients.html?id=${recipe.id}"><img src=${recipe.image} /></a>`;
+        searchResults.appendChild(newItem);
+      });
     });
+  });
 });
